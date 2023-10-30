f7a6c2be0ad1bf1e1f0a0a18d8916d2cd0eb1b73
Lucas Ornelas <130519452+Lucasornelas27@users.noreply.github.com>
2023-10-29 22:57:57 -0300

    Add files via upload

---
+++ porcentagem.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class porcentagem {
+    public static void main(String[]args){
+
+                    double a=100;
+                   double d;
+
+                                    Scanner sc = new Scanner(System.in);
+
+                                    System.out.println("porcento");
+                                    double b = sc.nextDouble();
+                                    System.out.println("a saber a porcentagem.");
+                                    double c = sc.nextDouble();
+                                    d=b*c/a;
+                                    System.out.println(d);
+
+
+    }
+}
