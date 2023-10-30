79f1afbea1cf51301ab86581173bab4ba6858066
Lucas Ornelas <130519452+Lucasornelas27@users.noreply.github.com>
2023-10-29 23:00:42 -0300

    Add files via upload

---
+++ Decisao.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Decisao {
+    public static void main(String[]args){
+    int a, i;
+    Scanner ler = new Scanner(System.in);
+        System.out.println("qual sua idade?");
+        i=ler.nextInt();
+        a=i;
+        if (a>=18){
+        System.out.println("adulto");
+        } else{
+        System.out.println("criança ou adolescente");
+        }
+    }
+}
